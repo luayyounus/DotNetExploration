ad09c560eb8908382bebd623c642619df7c94d62
Luay Younus <luayghazwan@yahoo.com>
2017-12-10 18:21:31 -0800

    While statements with two Games

--- MicrosoftVirtualAcademy/Prework.cs
+++ MicrosoftVirtualAcademy/Prework.cs
@@ -140,5 +140,86 @@ namespace MicrosoftVirtualAcademy
                 Console.ReadLine();
             }
         }
+
+        internal class WhileIteration
+        {
+            private static void Main(string[] args)
+            {
+                bool displayMenu = true;
+                while (displayMenu)
+                {
+                    displayMenu = MainMenu();
+                }
+            }
+
+            private static bool MainMenu()
+            {
+                Console.Clear();
+                Console.WriteLine("Choose an option:");
+                Console.WriteLine("1) Print Numberz Game");
+                Console.WriteLine("2) Guessing Game");
+                Console.WriteLine("3) Exit");
+
+                string result = Console.ReadLine();
+                if (result == "1")
+                {
+                    PrintNumbers();
+                    return true;
+                }
+                if (result == "2")
+                {
+                    GuessingGame();
+                    return true;
+                }
+                if (result == "3")
+                {
+                    return false;
+                }
+                return true;
+            }
+
+            private static void PrintNumbers()
+            {
+                Console.Clear();
+                Console.WriteLine("Print Numberz Game!");
+                Console.Write("Type a number: ");
+                int result = int.Parse(Console.ReadLine());
+                int counter = 1;
+                while (counter <= result)
+                {
+                    Console.Write(counter);
+                    Console.Write("-");
+                    counter++;
+                }
+                Console.ReadLine();
+            }
+
+            private static void GuessingGame()
+            {
+                Console.Clear();
+                Console.WriteLine("Guessing Game!");
+
+                Random myRandom = new Random();
+                int randomNumber = myRandom.Next(1, 11);
+
+                int guesses = 0;
+                bool incorrect = true;
+
+                do
+                {
+                    Console.WriteLine("Guess a number between 1 and 10: ");
+                    string result = Console.ReadLine();
+                    guesses++;
+                    if (result == randomNumber.ToString())
+                        incorrect = false;
+                    else
+                        Console.WriteLine("Wrong!");
+                    
+                } while (incorrect);
+
+                Console.WriteLine("Correct! It took you {0} guesses.", guesses);
+                Console.ReadLine();
+            }
+        }
     }
 }
