94c947893c0889486cc4ecc6819ae624e1b9f3c4
Luay Younus <luayghazwan@yahoo.com>
2017-12-10 17:04:48 -0800

    Decision

--- MicrosoftVirtualAcademy/Program.cs
+++ MicrosoftVirtualAcademy/Program.cs
@@ -26,4 +26,42 @@ namespace MicrosoftVirtualAcademy
             Console.ReadLine();
         }
     }
+
+    internal class Decision
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Christmas presents!");
+            Console.Write("Choose a door to open: 1, 2: ");
+
+            var userValue = Console.ReadLine();
+
+            switch (userValue)
+            {
+                case "1":
+                {
+                    const string message = "You won an Apple! :)";
+                    Console.WriteLine(message);
+                    break;
+                }
+                case "2":
+                {
+                    const string message = "You won a TV!";
+                    Console.WriteLine(message);
+                    break;
+                }
+                default:
+                    Console.WriteLine("You Lost! hehehe");
+                    break;
+            }
+
+            Console.WriteLine("Hit enter if you want to play again!");
+
+            var finalRound = Console.ReadLine();
+            var msg = (finalRound == "") ? "Oopsy, you fell in the trap, Bye!" : "Thanks for typing in something but no more games!";
+            //{0} is a replacement code/character
+            Console.Write("GAME RESPONSE: {0}", msg);
+            Console.ReadLine();
+        }
+    }
 }
