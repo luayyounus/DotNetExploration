c6d7051d00c12a8ee4b4a4b7d8938968571293e3
Luay Younus <luayghazwan@yahoo.com>
2017-12-10 17:03:47 -0800

    Variables

---
+++ MicrosoftVirtualAcademy/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicrosoftVirtualAcademy
+{
+    internal class Variables
+    {
+        private static void Main(string[] args)
+        {
+            const int x = 7;
+            const int y = x + 3;
+
+            Console.WriteLine(y);
+
+            Console.WriteLine("What is your name?");
+            Console.Write("Type your first name: ");
+            var myFirstName = Console.ReadLine();
+
+            Console.Write("Type your last name: ");
+            var myLastName = Console.ReadLine();
+
+            Console.WriteLine($"Hello {myFirstName} {myLastName}");
+            Console.ReadLine();
+        }
+    }
+}
